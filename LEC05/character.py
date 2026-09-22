57357cf98cb25394259463457e8722702a3990b5
chowon <2025184020a@tukorea.ac.kr>
2026-09-22 16:15:48 +0900

    개발 시작. 전체 구조 무한 루프 작성

--- LEC05/character.py
+++ LEC05/character.py
@@ -1,3 +1,5 @@
+from math import cos, sin, sqrt
+
 from pico2d import *
 
 
@@ -5,13 +7,29 @@ open_canvas(800, 600)
 
 # 여기를 채우시오.
 
+grass = load_image('grass.png')
+character = load_image('character.png')
 
+x = 400
+y = 300
+r = 100
+pi = 3.141592
+theta = 0
+while 1:
+    events = get_events()
+    # 게임 상호작용
+    x = 400 + r * cos(theta)
+    y = 300 + r * sin(theta)
+    theta += 0.02
 
+    # 게임 보여주기
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, y)
+    update_canvas()
 
+    delay(0.01)
 
-
-
-delay(2)
 
 close_canvas()
 

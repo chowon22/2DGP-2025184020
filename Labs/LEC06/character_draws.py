26fc3f3c2c648c6ac3cc95bf29a81f4888d44d45
chowon <2025184020a@tukorea.ac.kr>
2026-09-22 16:31:58 +0900

    원, 사각, 삼각 운동으로 이루어지는 전체 흐름 완성

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,3 +1,19 @@
 # 실습 과제 진행
+
+def MoveCircle():
+    print("circle")
+    pass
+
+def MoveRectangle():
+    print("Rectangle")
+    pass
+
+def MoveTriangle():
+    print("Triangle")
+    pass
+
 while 1:
+    MoveCircle()
+    MoveRectangle()
+    MoveTriangle()
     pass
